8f30e67a9239d5968e04b330951b4d339d565844
Rajeev Reddy <rajeevreddy@Rajeevs-MacBook-Pro.local>
2023-09-10 18:25:20 +0530

    github tests added

--- chittabook/tests.py
+++ chittabook/tests.py
@@ -40,3 +40,4 @@ class UsersManagersTests(TestCase):
             pass
         with self.assertRaises(ValueError):
             User.objects.create_superuser(email="super@user.com", password="foo", is_superuser=False)
+
