416db1fab243612cdd86973ffc4a606fe6bfe1ae
MMEGS <rajeevreddyms5@gmail.com>
2023-09-20 16:30:41 +0530

    added page under construction for all links

--- chittabook/urls.py
+++ chittabook/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("home/", views.home, name="home"),
+    path("how/", views.how, name="how"),
+    path("about/", views.about, name="about"),
+    path("features/", views.features, name="features"),
+    path("contact/", views.contact, name="contact"),
+    path("terms/", views.terms, name="terms"),
+    path("privacy/", views.privacy, name="privacy"),
 ]
--- chittabook/views.py
+++ chittabook/views.py
@@ -8,6 +8,37 @@ def index(request):
     return render(request, 'chittabook/index.html')
 
 
+# about page view for chittabook app
+def about(request):
+    return render(request, 'chittabook/about.html')
+
+
+# contact page view for chittabook app
+def contact(request):
+    return render(request, 'chittabook/contact.html')
+
+# features page view for chittabook app
+def features(request):
+    return render(request, 'chittabook/features.html')
+
+# how page view for chittabook app
+def how(request):
+    return render(request, 'chittabook/how.html')
+
+# privacy page view for chittabook app
+def privacy(request):
+    return render(request, 'chittabook/privacy.html')
+
+# terms and conditions page view for chittabook app
+def terms(request):
+    return render(request, 'chittabook/terms.html')
+
+
+
+''' home page view for chittabook app starts here'''
+
+
+
 # home page view for chittabook app
 @login_required
 def home(request):
