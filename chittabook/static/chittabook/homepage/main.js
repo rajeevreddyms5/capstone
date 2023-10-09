7292729a1090ae26f4b84c9cccb5ae2dfea9eb77
Rajeev Reddy <rajeevreddy@Rajeevs-MacBook-Pro.local>
2023-10-09 08:54:03 +0530

    dropdown menu working in sidebar

--- chittabook/static/chittabook/homepage/main.js
+++ chittabook/static/chittabook/homepage/main.js
@@ -5,12 +5,17 @@ document.addEventListener('DOMContentLoaded', function () {
     dropdowns.forEach((dropdown) => {
         dropdown.addEventListener("click", function () {
             const icon = dropdown.querySelector("i");
+            const items = dropdown.nextElementSibling;
         if (icon.classList.contains("fa-chevron-down")) {
             icon.classList.remove("fa-chevron-down");
             icon.classList.add("fa-chevron-up");
+            if (items.style.display) {
+                items.style.display = "block";
+            }
         } else {
             icon.classList.remove("fa-chevron-up");
             icon.classList.add("fa-chevron-down");
+            items.style.display = "none";
         }
             })
     })
