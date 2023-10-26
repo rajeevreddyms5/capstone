b24bf6e2f2dabd545645a2e034c09e4ccd371ba8
Rajeev Reddy <rajeevreddy@Rajeevs-MacBook-Pro.local>
2023-10-26 08:16:12 +0530

    just saving after changes in expense category

--- chittabook/models/expense.py
+++ chittabook/models/expense.py
@@ -1,12 +1,12 @@
 from django.db import models
 from .usermodel import User
-from .accounts import BankAccount, LoanAccount, CreditCards, SavingsAccount
+from .accounts import BankAccount, LoanAccount, CreditCards, InvestmentAccount
 from django.utils.timezone import localtime
 
 # expense category
 class ExpenseCategory(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	name = models.CharField(max_length = 256)
+	name = models.CharField(max_length=256)
 	created_at = models.DateTimeField(default=localtime)
 
 	def __str__(self):
@@ -18,28 +18,33 @@ class ExpenseCategory(models.Model):
 
 # expense
 class Expense(models.Model):
-	
-    # account_choices from BankAccount, LoanAccount, CreditCards, SavingsAccount
-    account_choices_list = (
-		BankAccount.objects.all(),
-        LoanAccount.objects.all(),
-        CreditCards.objects.all(),
-        SavingsAccount.objects.all(),
-    )
-	
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-	account = models.ForeignKey(choices=account_choices_list, on_delete=models.CASCADE)
-	amount = models.FloatField()
-	date = models.DateField(default = localtime)
+	# choices
+	BankAccounts = BankAccount.objects.all()
+	LoanAccounts = LoanAccount.objects.all()
+	CreditCards = CreditCards.objects.all()
+	InvestmentAccount = InvestmentAccount.objects.all()
+
+	# account_choices from BankAccount, LoanAccount, CreditCards, InvestmentAccount
+	ACCOUNT_CHOICES = [
+		('BankAccounts', 'Bank Accounts'),
+		('LoanAccounts', 'Loan Accounts'),
+		('CreditCards', 'Credit Cards'),
+		('InvestmentAccounts', 'Investment Accounts'),
+	]
+
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	account = models.CharField(max_length=256, choices=ACCOUNT_CHOICES)
+	amount = models.DecimalField(max_digits=10, decimal_places=2)
+	date = models.DateField(default=localtime)
 	description = models.TextField()
 	category = models.ForeignKey(ExpenseCategory, on_delete=models.CASCADE)
 	created_at = models.DateTimeField(default=localtime)
 
 	def __str__(self):
-		return str(self.category) + str(self.date )+ str(self.amount)
+		return str(self.category) + str(self.date) + str(self.amount)
 
 	class Meta:
-		ordering:['-date']
+		ordering = ['-date']
 
 
 
